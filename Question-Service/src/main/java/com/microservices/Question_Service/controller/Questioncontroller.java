37f30e368665c047cf10239b1362e541247af10d
SatyaPrakash2023 <saratkumarsharma01@gmail.com>
2026-06-11 00:29:57 +0530

    feat(question-service): implement core question management APIs
    - POST /questions/add - add a new question
    
    - GET /questions/allQuestions - fetch all questions
    
    - POST /questions/getQuestion - fetch questions by IDs

---
+++ Question-Service/src/main/java/com/microservices/Question_Service/controller/Questioncontroller.java
@@ -0,0 +1,64 @@
+package com.microservices.Question_Service.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservices.Question_Service.model.Question;
+import com.microservices.Question_Service.model.QuestionWrapper;
+import com.microservices.Question_Service.model.Response;
+import com.microservices.Question_Service.service.QuestionService;
+
+
+@RestController
+@RequestMapping("/questions")
+public class Questioncontroller {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("/allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("/addQuestion")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+
+    //Generate quiz
+    //GetQuestion(QuestionId)
+    //GetScore
+
+    @GetMapping("/generate")
+    public ResponseEntity<List<Integer>> generateQuestionForQuiz(@RequestBody String categoryName,@RequestParam Integer numQuestions) {
+        return questionService.getQuestionsForQuiz(categoryName, numQuestions);
+    }
+
+    @PostMapping("/getQuestion")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionFromId(@RequestBody List<Integer> questionsId) {
+        // Implementation for fetching a specific question
+        return questionService.getQuestionFromId(questionsId);
+    }
+
+    @PostMapping("/getScore")
+    public ResponseEntity<String> getScore(@RequestParam List<Response> responses) {
+        // Implementation for calculating the score based on user answers
+        return questionService.getScore(responses);
+    }
+
+}
---
+++ Question-Service/src/main/java/com/microservices/Question_Service/service/QuestionService.java
@@ -0,0 +1,83 @@
+package com.microservices.Question_Service.service;
+
+import java.io.ObjectInputFilter.Status;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.microservices.Question_Service.dao.QuestionDao;
+import com.microservices.Question_Service.model.Question;
+import com.microservices.Question_Service.model.QuestionWrapper;
+import com.microservices.Question_Service.model.Response;
+
+@Service
+public class QuestionService {
+
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        questionDao.save(question);
+        return new ResponseEntity<>("success",HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(String categoryName, Integer numQuestions) {
+        // TODO Auto-generated method stub
+        List<Integer> question=questionDao.findRandomQuestionsByCategory(categoryName,numQuestions);
+        return new ResponseEntity<>(question, HttpStatus.OK);
+    }
+
+    
+    public ResponseEntity<List<QuestionWrapper>> getQuestionFromId(List<Integer> questionsId) {
+        List<QuestionWrapper> questionWrappers = new ArrayList<>();
+        List<Question> questions = questionDao.findAllById(questionsId);
+        for (Question question : questions) {
+            QuestionWrapper wrapper = new QuestionWrapper();
+            wrapper.setId(question.getId());
+            wrapper.setQuestionTitle(question.getQuestionTitle());
+            wrapper.setOption1(question.getOption1());
+            wrapper.setOption2(question.getOption2());
+            wrapper.setOption3(question.getOption3());
+            wrapper.setOption4(question.getOption4());
+            questionWrappers.add(wrapper);
+        }
+        return new ResponseEntity<>(questionWrappers, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> getScore(List<Response> responses) {
+        // TODO Auto-generated method stub
+        int right=0;
+        for(Response response: responses){
+            Question question=questionDao.findById(response.getId()).orElse(null);
+            if(question!=null && question.getRightAnswer().equals(response.getResponse())){
+                right++;
+            }
+        }
+        throw new UnsupportedOperationException("Unimplemented method 'getScore'");
+    }
+}
